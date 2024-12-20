0c596e96a8c391e6dcc8bffaac5f63d16c1c7db2
Aurora Jitrskul <aurawra@outlook.com>
2024-12-19 22:30:22 -0600

    Implemented restaurant retrieval

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify, request
 import requests
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
 
 @app.route('/api/data', methods=['POST'])
 def getRestaurants():
+  print('hi')
+
   # Parameters for the API request
   params = request.get_json()
+  print(params)
 
   API_KEY = 'v1JO42CoSqmHEt7Pc-_E2NKdMOKqrNCXzeN90AgBbPyG8pci5NRNLP9VPtc-83zRWOMJRMDHodKW-HoOJBoXh9sapAGpEJcCR3hrMfDGR_3pacWRMardVn-DgdVdZ3Yx'
   HEADERS = {'Authorization': f'Bearer {API_KEY}'}
@@ -20,9 +26,13 @@ def getRestaurants():
 
   # Make the request to the Yelp API
   response = requests.get(url, headers=HEADERS, params=params)
-  print(response.text)
 
-  return "Hello, World!"
+  # Parse the JSON response data
+  data = response.json()
+  
+  print(data['businesses'])
+
+  return jsonify(data['businesses'])
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5001, debug=True)
--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,4 @@
-function renderMain() {
-
-}
+let restaurants = []
 
 async function getLocation() {
   return new Promise((resolve, reject) => {
@@ -22,19 +20,20 @@ async function getLocation() {
 }
 
 async function getYelpData(params) {
-  const response = await fetch("http://127.0.0.1:5001/api/data", {
+
+  try {
+    const response = await fetch("http://127.0.0.1:5001/api/data", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(params)
-  });
+    })
 
-  if (response.ok) {
-      const data = await response.json();
-      console.log(data);  // Display the Yelp data
-  } else {
-      console.error('Failed to fetch Yelp data');
+    return response.json();
+  }
+  catch (error) {
+    console.log("Error!");
   }
 }
 
@@ -64,22 +63,13 @@ document.getElementById('user-input').addEventListener('submit', async function(
       term: 'restaurants',
       latitude: location.latitude, 
       longitude: location.longitude,
-      radius: formData.get('num-miles'),
-      limit: (formData.get('num-restaurants')*1609), //calc meters in mile
+      radius: (formData.get('num-miles')*1609),
+      limit: formData.get('num-restaurants'), //calc meters in mile
       open_now: true,
       price: prices
     }
 
-    getYelpData(params);
+    restaurants = await getYelpData(params);
+    
   }
-  // const response = await fetch('https://example.com/submit', {
-  //     method: 'POST',
-  //     body: formData,
-  // });
-
-  // if (response.ok) {
-  //     console.log('Form submitted successfully!');
-  // } else {
-  //     console.error('Form submission failed!');
-  // }
 });
